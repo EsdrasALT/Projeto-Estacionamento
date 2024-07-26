d573c663ebc75306fb4cc491446d3f62ae553756
EsdrasALT <esdras.a.pessoal@gmail.com>
2024-07-26 19:33:55 -0300

    arquivos do projeto

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include "biblioteca.h"
+
+//Daniel Ribeiro Costa
+//Danilo Augusto
+//Esdras Altivo
+
+//Criado, testado e executado utilizando a IDE Eclipse c c++
+
+int main(void) {
+	setbuf(stdout, NULL);
+
+	menuPrincipal();
+
+	return 0;
+}
